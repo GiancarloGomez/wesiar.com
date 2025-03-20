d6d862c48e900b9aa57634f1dc8e3680562aba36
Giancarlo Gomez <giancarlo.gomez@gmail.com>
2025-03-20 14:30:54 -0400

    Some more changes for iOS

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,6 @@
 const taglines = document.querySelectorAll(".fade-text");
 let index = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(() => {
-        document.getElementById("main-heading").classList.add("show");
-        setTimeout(() => {
-            taglines[index].classList.add("show");
-            setTimeout(startTaglineLoop, 5000); // Wait before starting loop
-        }, 1500);
-    }, 500);
-});
-
 function startTaglineLoop() {
     setInterval(() => {
         taglines[index].classList.remove("show");
@@ -18,3 +8,31 @@ function startTaglineLoop() {
         taglines[index].classList.add("show");
     }, 6000); // Change tagline every 6 seconds
 }
+
+function appHeight(){
+    const doc = document.documentElement;
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+}
+
+function debounce(func, wait) {
+    let timeout;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.getElementById("main-heading").classList.add("show");
+        setTimeout(() => {
+            document.querySelector(".fade-container").classList.add("on");
+            taglines[index].classList.add("show");
+            setTimeout(startTaglineLoop, 5000); // Wait before starting loop
+        }, 1500);
+    }, 500);
+});
+
+window.addEventListener('resize', debounce(appHeight, 250));
